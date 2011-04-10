9fe74ba34720c3831b29bce344a6d96bf30c407b
Erik Krasner-Karpen <erik.krasnerkarpen@gmail.com>
2011-04-10 11:55:13 -0400

    cleaned up code for prime_gen with sweet mutual recursion

--- prime_gen.py
+++ prime_gen.py
@@ -24,24 +24,9 @@ def prime(n):
         return n in prime_set
     if not n % 2 or n == 1:
         return False
-    for p in takewhile(lambda x: x * x <= n, prime_list):
+    for p in takewhile(lambda x: x * x <= n, primes()):
         if not n % p:
             return False
-    while prime_list[-1] * prime_list[-1] < n:
-        try:
-            next_prime = next(file_iter)
-            prime_set.add(next_prime)
-            prime_list.append(next_prime)
-            if not n % next_prime:
-                return False
-        except StopIteration:
-            for odd_number in count(prime_list[-1] + 2,2):
-                if prime(odd_number):
-                    prime_set.add(odd_number)
-                    prime_list.append(odd_number)
-                    if not n % odd_number:
-                        return False
-                    break
     return True
 
 def primes():
